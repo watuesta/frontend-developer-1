a698e22460567a900685fd2254406461c201c6b8
William Atuesta <watuesta@Williams-MacBook-Pro.local>
2023-01-20 19:11:41 -0500

    Archivo primer clase curso basico JS

---
+++ basic_javascript/valores.js
@@ -0,0 +1,20 @@
+//Numericos
+40
+
+//Cadena de texto
+"William Atuesta"
+
+//Booleanos
+true
+false
+
+//Valores reservados
+null
+undefined
+
+//Todos los anteriores son los valores primitivos
+
+//Valores no primitivos
+[1,2,3]
+//Valores tipo objetos
+{ nombre: "William"}
