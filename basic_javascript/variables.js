e11edc6c68db10051fa26f9eeb35bcfa83f05650
William Atuesta <watuesta@Williams-MacBook-Pro.local>
2023-01-20 19:20:39 -0500

    Segunda clase Basico JS

---
+++ basic_javascript/variables.js
@@ -0,0 +1,13 @@
+var nombre = "Will";
+
+// Declarar
+var edad;
+// Inicializar
+edad = 31;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre : "William",
+    edad: 30
+}
