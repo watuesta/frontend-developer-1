953bf090de5ac882b7abcfa4ebe38d1ea7237fb4
William Atuesta <watuesta@Williams-MacBook-Pro.local>
2023-01-23 13:47:41 -0500

    Se adjunta archivo de clase de IF JS Básico

---
+++ basic_javascript/if.js
@@ -0,0 +1,18 @@
+// if simple
+if (false){
+    console.log("Hola");
+}else{
+    console.log("Soy falso");
+}
+
+// if escalonado
+
+var edad = 18;
+
+if (edad === 18){
+    console.log("Puedes votar, será tu primera votación");
+}else if(edad > 18){
+    console.log("Puedes votar de nuevo");
+}else{
+    console.log("Aún no puedes votar");
+}
