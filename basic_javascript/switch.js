b5dcb80816821de168a1c760645c2c25fd8988ea
William Atuesta <w.atuesta@gmail.com>
2023-01-24 13:13:00 -0500

    Se adiciona clase de Switch JS Básico

---
+++ basic_javascript/switch.js
@@ -0,0 +1,12 @@
+var numero = 1;
+
+switch(numero){
+    case 1:
+        console.log("Soy 1");
+        break;
+    case 2:
+        console.log("Soy 2");
+        break;
+    default:
+        console.log("No soy nada");
+}
