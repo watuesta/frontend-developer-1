a0a42bfbbd334bbdecc09cd709c428992075bdb4
William Atuesta <w.atuesta@gmail.com>
2023-01-30 13:06:50 -0500

    Se envia archivo con clase de arrays JS Básico

---
+++ basic_javascript/arrays.js
@@ -0,0 +1,27 @@
+
+var frutas = ["Manzana", "Plátano", "Cereza", "Fresa"];
+
+// Devuelve todos los elementos del array
+console.log(frutas);
+
+// Longitud del array
+console.log(frutas.length);
+
+// Llamar un valor de array
+console.log(frutas[2]);
+
+// Cómo meter más elementos al array con push
+var masFrutas = frutas.push("Uvas");
+
+// Como eliminar el último elemento de un array pop
+var ultimo = frutas.pop("Uvas");
+
+// Agregar un elemento al inicio del array
+var nuevaLongitud = frutas.unshift("Uvas");
+
+// Eliminar el primer elemento de un array
+var borrarFruta = frutas.shift("Uvas");
+
+// Saber la posición de un elemento del array
+var posicion = frutas.indexOf("Cereza");
+
