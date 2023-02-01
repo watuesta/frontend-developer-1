28a839da09bf3d84328b8f558ebde94f2e027d5e
William Atuesta <w.atuesta@gmail.com>
2023-01-31 20:33:58 -0500

    Clase de objetos con ejercicio práctico JS Básico

--- basic_javascript/objetos.js
+++ basic_javascript/objetos.js
@@ -8,3 +8,33 @@ var miAuto = {
         console.log("Auto " + this.modelo + " " + this.anio);
     }
 };
+
+// Función constructora
+function auto(marca, modelo, anio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+}
+
+var autoNuevo = new auto("Tesla", "Model 3", 2020);
+var autoNuevo2 = new auto("Tesla", "Model X", 2018);
+var autoNuevo3 = new auto("Toyota", "Corolla", 2020);
+
+//Ejercicio en clase ciclo para crear instancias de objeto.
+function auto (MARCA, MODELO, ANNIO){
+    this.marca = MARCA;
+    this.modelo = MODELO;
+    this.annio = ANNIO;
+  }
+  var autos = [];
+  for(let i = 0 ; i < 30 ; i++){
+    var marca = prompt("Ingresa la marca del auto");
+    var modelo = prompt("Ingresa el modelo del auto");
+    var annio = prompt("Ingresa el año del auto");
+    autos.push(new auto (marca, modelo, annio));
+  }
+  
+  for(let i = 0 ; i < autos.length ; i++){
+    console.log(autos[i]);
+  }
+  
